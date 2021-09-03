56cb43a4afd97a72db6815562eca4a36907abf1b
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-09-03 14:09:51 -0500

    adds home route

--- src/main/resources/static/js/router.js
+++ src/main/resources/static/js/router.js
@@ -1,11 +1,18 @@
-
 /**
  * Returns the route object for a specific route based on the given URI
  * @param URI
  * @returns {*}
  */
+import Home from "./views/Home";
+
 export default function router(URI) {
     const routes = {
+        '/': {
+            returnView: Home,
+            state: {},
+            uri: '/',
+            title: 'Home',
+        }
 
     };
 
