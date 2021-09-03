3c78972d964543f38b4b259d50949185b9c263d5
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-09-03 14:23:09 -0500

    adds render

--- src/main/resources/static/js/render.js
+++ src/main/resources/static/js/render.js
@@ -1,9 +1,11 @@
-
 /**
  * Pushes the current URI to the URL bar and sets the HTML of the app div.
  * @param props - the data required for view rendering
  * @param route - the object containing information for the given endpoint
  */
+import Navbar from "./views/partials/Navbar";
+import Footer from "./views/partials/Footer";
+
 export default function render(props, route) {
     const app = document.querySelector('#app');
     const title = `REST Blog - ${route.title}`;
@@ -12,7 +14,7 @@ export default function render(props, route) {
 
     // add view and navbar to DOM
     app.innerHTML = `${Navbar(null)} ${route.returnView(props)}`;
-
+    app.innerHTML = `${Footer(null)} ${route.returnView(props)}`;
     // add events AFTER view is added to DOM
     if (route.viewEvent){
         route.viewEvent();
