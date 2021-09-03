a4334fbc68a01cb91afd988ab95a9b2501e601d3
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-09-03 14:45:51 -0500

    adds input for workouts

--- src/main/resources/static/js/views/Workouts.js
+++ src/main/resources/static/js/views/Workouts.js
@@ -5,9 +5,26 @@ export default function Workouts(props) {
         </header>
         <main>
             <div>
-                <p>
-                    This is the workout page text.
-                </p>    
+               <form>
+  <input type="checkbox" id="shoulders" name="vehicle1" value="Bike">
+  <label for="vehicle1"> I have a bike</label><br>
+  <input type="checkbox" id="chest" name="vehicle2" value="Car">
+  <label for="vehicle2"> I have a car</label><br>
+  <input type="checkbox" id="biceps" name="vehicle3" value="Boat">
+  <label for="vehicle3"> I have a boat</label>
+  <input type="checkbox" id="triceps" name="vehicle1" value="Bike">
+  <label for="vehicle1"> I have a bike</label><br>
+  <input type="checkbox" id="abs" name="vehicle2" value="Car">
+  <label for="vehicle2"> I have a car</label><br>
+  <input type="checkbox" id="ass" name="vehicle3" value="Boat">
+  <label for="vehicle3"> I have a boat</label>
+  <input type="checkbox" id="quads" name="vehicle1" value="Bike">
+  <label for="vehicle1"> I have a bike</label><br>
+  <input type="checkbox" id="hammies" name="vehicle2" value="Car">
+  <label for="vehicle2"> I have a car</label><br>
+  <input type="checkbox" id="caffs" name="vehicle3" value="Boat">
+  <label for="vehicle3"> I have a boat</label>
+</form>
             </div>
         </main>
     `;
