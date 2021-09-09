cea0b40561319b3f0071d917ac5f77c6d00a1157
richardlara35 <richard.u.lara35@gmail.com>
2021-09-09 16:55:23 -0500

    profile page

---
+++ src/main/resources/static/js/views/Profile.js
@@ -0,0 +1,65 @@
+import createView from "../createView";
+
+
+export default function Profile(props) {
+    return `
+        <header>
+            <h1>Profile Page</h1>
+        </header>
+        <main>
+            <div id="playlist">
+                <p>
+                    This is your Profile page.
+                </p>    
+                ${getPlaylists}
+            </div>
+        </main>
+    `;
+}
+
+export function playlistEvent(){
+    deletePlaylist();
+}
+
+
+function getPlaylists(playlist) {
+    for (let i = 0; i < playlist.length; i++) {
+
+        $('#playlist').append(`
+      
+                <div class="playlist">
+                    <span class="name">${playlist.name}</span>
+                    <span class="bodypart">${playlist.bodyPart}</span>
+                    <span class="equipment">${playlist.equipment}</span>
+                    <span class="muscle">${playlist.primary_muscle}</span>
+                    <span class="gif">${playlist.gif_url}</span>
+                    <span class="rating">${playlist.rating}</span>
+                    <div>
+                    </div>
+                    <button class="delete-playlist-btn" data-id=${playlist.id}>Delete</button>
+        </div>
+                
+    `)
+    }
+}
+
+function deletePlaylist() {
+    $(".delete-playlist-btn").click(function (){
+        let request = {
+            method: "DELETE",
+            headers: {"Content-Type":"application/json"},
+        }
+        let id = $(this).attr("data-id");
+
+
+        fetch(`http://localhost:8080/api/${id}`, request)
+            .then(res => {
+                console.log(res.status);
+                createView("/profile")
+            }).catch(error => {
+            console.log(error);
+            createView("/profile")
+        });
+
+    })
+}
