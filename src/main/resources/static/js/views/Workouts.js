f95919ecf63fc0a16fdd11af4e494328bfdcbaa3
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-09-09 15:56:56 -0500

    adds to workouts

--- src/main/resources/static/js/views/Workouts.js
+++ src/main/resources/static/js/views/Workouts.js
@@ -1,5 +1,7 @@
+import createView from "../createView.js";
+
 export default function Workouts(props) {
-	return `
+    return `
         <header>
             <h1>Workout Page</h1>
         </header>
@@ -31,67 +33,67 @@ export default function Workouts(props) {
 
 
 export function getBodyPart() {
-	$(".submit-btn")
-		.click(function () {
-			let selectOption = $("#bodyParts :selected")
-				.val();
+    $(".submit-btn")
+        .click(function () {
+            let selectOption = $("#bodyParts :selected")
+                .val();
 
 
-			console.log(selectOption)
+            console.log(selectOption)
 
 
-			fetch(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${selectOption}`, {
-				"method": "GET",
-				"headers": {
-					"x-rapidapi-host": "exercisedb.p.rapidapi.com",
-					"x-rapidapi-key": "0fda1c2912msh4a1299d685685e4p139959jsn8a0fbfe6c5a1"
-				}
-			})
-				.then(response => {
-					return (response.json());
-				})
-				.then(function (data) {
-					console.log(data)
-					appendAllWorkoutData(filterWorkoutObject(data))
-				})
-				.catch(err => {
-					console.error(err);
-				});
+            fetch(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${selectOption}`, {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+                    "x-rapidapi-key": "0fda1c2912msh4a1299d685685e4p139959jsn8a0fbfe6c5a1"
+                }
+            })
+                .then(response => {
+                    return (response.json());
+                })
+                .then(function (data) {
+                    console.log(data)
+                    appendAllWorkoutData(filterWorkoutObject(data))
+                })
+                .catch(err => {
+                    console.error(err);
+                });
 
-		})
+        })
 
 }
 
 function filterWorkoutObject(data) {
-	let workoutObjArr = [];
-	for (let i = 0; i < data.length; i++) {
-		workoutObjArr.push({
-			bodyPart: data[i].bodyPart,
-			equipment: data[i].equipment,
-			gifUrl: data[i].gifUrl,
-			id: data[i].id,
-			name: data[i].name,
-			target: data[i].target
-		})
-	}
-	return workoutObjArr;
+    let workoutObjArr = [];
+    for (let i = 0; i < data.length; i++) {
+        workoutObjArr.push({
+            bodyPart: data[i].bodyPart,
+            equipment: data[i].equipment,
+            gifUrl: data[i].gifUrl,
+            id: data[i].id,
+            name: data[i].name,
+            target: data[i].target
+        })
+    }
+    return workoutObjArr;
 }
 
 function appendAllWorkoutData(workoutArr) {
-	$('#workout-container')
-		.empty()
-	workoutArr.forEach(function (obj) {
-		$('#workout-container')
-			.append(getWorkoutCard(obj))
-	})
+    $('#workout-container')
+        .empty()
+    workoutArr.forEach(function (obj) {
+        $('#workout-container')
+            .append(getWorkoutCard(obj))
+    })
 }
 
 
 function getWorkoutCard(workoutObj) {
-	let workoutsCard = $(`<div class="card col-lg-3 px-3 mb-2 mt-2"></div>`);
+    let workoutsCard = $(`<div class="card col-lg-3 px-3 mb-2 mt-2"></div>`);
 
-	workoutsCard.append(
-		`<form>
+    workoutsCard.append(
+        `<form>
 		<input class="card-header" id="bodyPart" readonly>${workoutObj.bodyPart}</input>
 		<input id="name" readonly>${workoutObj.name}</input>
 		<input id="equipment" readonly>${workoutObj.equipment} </input>
@@ -107,15 +109,31 @@ function getWorkoutCard(workoutObj) {
 		<button id="workout-submit-btn" class="btn">Select</button></form>
 		
 		`
-	)
-	return workoutsCard
+    )
+    return workoutsCard
 }
 
 
 function addWorkoutEvent() {
-	$("#workout-submit-btn")
-		.click(function () {
+    $("#workout-submit-btn")
+        .click(function () {
+            let selectedWorkout = {
+                bodyPart: $("#bodyPart").val(),
+                name: $("#name").val(),
+                equipment: $("#equipment").val(),
+                target: $("#target").val(),
+                rating: $("#rating :selected").val()
+
+            }
+            let request = {
+                method: 'POST',
+                headers: {'Content-Type': 'application/api/workouts'},
+                body: JSON.stringify(selectedWorkout)
 
 
-		})
+            }
+            fetch("http://localhost:8080/api/workouts", request).then(createView("/posts"));
+        })
+
+
 }
