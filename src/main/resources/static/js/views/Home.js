a048b5cee10dcb4c68844364e280df85817e67a3
stephen <stephen.h.nguyen.19@gmail.com>
2021-09-15 14:19:48 -0500

    adds correction to track your calories header

--- src/main/resources/static/js/views/Home.js
+++ src/main/resources/static/js/views/Home.js
@@ -29,10 +29,10 @@ export default function Home(props) {
                         <p>Effortlessly use our workout page to view all workouts based off what body part you want to work out. We have a variety workouts to choose from!</p>                       
                         </a>
                     </div>
-                    <div class="col-lg-4 mb-5 mb-lg-0"">Track Your Calories</h2>
-                        <p>You can enter your data into the calculator and it will recommend you how many calories you need to intake based on your data.</p>
+                    <div class="col-lg-4 mb-5 mb-lg-0"">
                         <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
-                        <h2 class="h4 fw-bolder">                       
+                        <h2 class="h4 fw-bolder">Track Your Calories</h2>                       
+                        <p>You can enter your data into the calculator and it will recommend you how many calories you need to intake based on your data.</p>
                         </a>
                     </div>
                 </div>
