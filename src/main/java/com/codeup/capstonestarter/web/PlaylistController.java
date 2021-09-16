f36802c30c97569a44054224ed831d532357271a
cruby9339@gmail.com <christian.ruby72@gmail.com>
2021-09-16 09:58:28 -0500

    adds post method

--- src/main/java/com/codeup/capstonestarter/web/PlaylistController.java
+++ src/main/java/com/codeup/capstonestarter/web/PlaylistController.java
@@ -3,9 +3,7 @@ package com.codeup.capstonestarter.web;
 
 import com.codeup.capstonestarter.data.playlist.Playlist;
 import com.codeup.capstonestarter.data.playlist.PlaylistRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +20,11 @@ public class PlaylistController {
     @GetMapping
     private List<Playlist> getPlaylists(){
         return playlistRepository.findAll();
+    }
+
+    @PostMapping
+    private void createNewPlaylist(@RequestBody Playlist newPlaylist){
+        playlistRepository.save(newPlaylist);
     }
 
 
