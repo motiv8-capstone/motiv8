73af2f352c51a2aff2ecda1cfd46d0457a7d8e2e
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-09-17 16:58:47 -0500

    adds stuff

--- src/main/resources/static/js/views/partials/Navbar.js
+++ src/main/resources/static/js/views/partials/Navbar.js
@@ -1,6 +1,7 @@
 export default function Navbar(props) {
 	return `
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <ul class="navbar-nav">
             <li class="nav-item active">
             <a class="nav-link" href="/" data-link>Home</a>
