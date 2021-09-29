209f3bc3fd0b12a6e7a77ec101a88726980180e3
cruby9339@gmail.com <christian.ruby72@gmail.com>
2021-09-29 10:14:37 -0500

    adds updated home page

--- src/main/resources/static/js/views/Home.js
+++ src/main/resources/static/js/views/Home.js
@@ -18,18 +18,18 @@ export default function Home(props) {
         <section class="py-5 border-bottom" id="features">
             <div class="container px-5 my-5">
                 <div class="row gx-5"> 
-                    <div class="col-lg-4 mb-5 mb-lg-0 border border-dark rounded">
+                    <div class="col-lg-4 mb-5 mb-lg-0">
                         <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-collection"></i></div>
                         <h2 class="h4 fw-bolder">End Accountability Issues</h2>
                         <p>Having trouble remembering which workout to do next? Well, registered users can create playlist's of workouts and use them any time they need to!</p>
                     </div>
-                    <div class="col-lg-4 mb-5 mb-lg-0 border border-dark rounded">
+                    <div class="col-lg-4 mb-5 mb-lg-0">
                         <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-building"></i></div>
                         <h2 class="h4 fw-bolder">Need Help Planning A Workout?</h2>
                         <p>Effortlessly use our workout page to view all workouts based off what body part you want to work out. We have a variety workouts to choose from!</p>                       
                         </a>
                     </div>
-                    <div class="col-lg-4 border border-dark rounded">
+                    <div class="col-lg-4">
                         <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
                         <h2 class="h4 fw-bolder">Track Your Calories</h2>                       
                         <p>You can enter your data into the calculator and it will recommend you how many calories you need to intake based on your data.</p>                       
